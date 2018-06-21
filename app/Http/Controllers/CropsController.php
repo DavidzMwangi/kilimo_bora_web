fb2144d0b0b23a6f2a2740a984a5012754aad736
DavidzMwangi <mwangidavidwanjohi@gmail.com>
2018-06-21 10:08:23 +0300

    early changes

--- app/Http/Controllers/CropsController.php
+++ app/Http/Controllers/CropsController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\County;
+use App\Models\CountyCrop;
 use App\Models\Crop;
 use App\Models\SubCounty;
 use Illuminate\Http\Request;
@@ -37,7 +38,16 @@ class CropsController extends Controller
         $subcounties=SubCounty::where('county_id',$request->input('county_id'))->get();
 
         return response()->json([
-            'sub_counties'=>$subcounties
+            'sub_counties'=>$subcounties->load('county')
+        ]);
+    }
+
+    public function getSubCountyCropRecords(Request $request)
+    {
+        $records=CountyCrop::where('sub_county_id',$request->input('sub_county_id'))->get();
+
+        return response()->json([
+            'records'=>$records->load(['crop','subCounty'])
         ]);
     }
 }
